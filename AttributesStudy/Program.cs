4c11b04fab0da3c96fca31d2ed37384a98a415a3
Marcus Bianchi <marcusb@take.net>
2019-01-14 16:52:15 -0200

    Add project files.

---
+++ AttributesStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AttributesStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
